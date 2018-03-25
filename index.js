7aa086fff241e8549f4157aca2fff65b35f37196
SeanRobb <seanpatrickrobb@gmail.com>
2018-03-25 15:37:59 -0500

    Updating index to have methods easier to call.

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const account = require('./lib/account');
 const market = require('./lib/market');
 
+const getAccountDetail = (address) => {
+  return account.detail(address);
+};
+
+const getAccountTxList = (address) => {
+  return account.txList(address);
+};
+
+const getAccountHarvestBlocks = (address) => {
+  return account.harvestBlocks(address);
+};
+
+const getMarket = () => {
+  return market.get();
+};
+
+
 module.exports = {
-  account,
-  market,
+  getAccountDetail,
+  getAccountTxList,
+  getAccountHarvestBlocks,
+  getMarket,
 };
